fabc1dcccac723f2c989d01d3f79fec7eee6ba14
[Shashwat Singh] <[singhshashwat521@gmail.com]>
2024-05-30 23:08:11 +0530

    UI updated

--- grading/home.php
+++ grading/home.php
@@ -5,6 +5,6 @@
     text-shadow: 2px 8px 6px #0003, 0 -5px 16px #ffffff4d;">Government Polytechnic Adityapur</h1>
     <h1 class="" style="text-align:center; margin-top:5%; font-weight: 600; font-size:25px;
     color: #74353F;
-    text-shadow: 2px 8px 6px #0003, 0 -5px 16px #ffffff4d;"><u>Created by</u><br><br>Aashi Priya<br>Devanshu Kumar Prasad<br>Srishti Kishor<br>Vikash Kumar Singh<br>Shashwat Singh</h1>
+    text-shadow: 2px 8px 6px #0003, 0 -5px 16px #ffffff4d;"><u>Created by</u><br><br>Aashi Priya<br>Srishti Kishor<br>Devanshu Kumar Prasad<br>Vikash Kumar Singh<br>Shashwat Singh</h1>
 			
 			
